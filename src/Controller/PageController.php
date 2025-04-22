1a41564129e3f323a3478de91350fb482c8d2dc6
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-04-22 13:51:49 +0200

    mise a jour

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,15 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 
 //Je cree une class HomeController
-class HomeController{
+class PageController{
 
     // Je cree une methode home qui va afficher la page d'accueil
-    // Je lui donne une route /home et je l'appelle home
+    // Je lui donne une annotation Route /home et je l'appelle home
     // Je lui donne un nom home
     //je fais un var_dump de la page d'accueil et je fais un die pour arreter le script
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function home()
     {
         var_dump('page accueil');die;
     }
+
+   #[Route('/contact', name: 'contact')]
+   public function contact(){
+       var_dump('page contact');die;
+   }
+
+   #[Route('/about', name:'about')]
+   public function about(){
+       var_dump('page about');die;
+   }
 }
